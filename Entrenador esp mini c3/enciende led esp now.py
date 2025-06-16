222bd4fbb2c1377b0663b78a2e4686592444485a
electronica y diagramas <151683295+eliasmonzon@users.noreply.github.com>
2025-06-16 20:31:37 -0300

    Update enciende led esp now.py

--- Entrenador esp mini c3/enciende led esp now.py
+++ Entrenador esp mini c3/enciende led esp now.py
@@ -1,1 +1,28 @@
 
+import network
+import espnow
+from machine import Pin
+
+# Configuración del LED
+led = Pin(2, Pin.OUT)
+estado_led = False  # Estado actual del LED (apagado al inicio)
+
+# Activar WiFi en modo estación
+sta = network.WLAN(network.STA_IF)
+sta.active(True)
+
+# Inicializar ESP-NOW
+esp = espnow.ESPNow()
+esp.active(True)
+
+print("Esperando mensajes...")
+
+while True:
+    host, msg = esp.recv()
+    if msg:
+        print("Recibido:", msg)
+        comando = msg.decode('utf-8').strip().lower()
+        if comando == 'on':
+            estado_led = not estado_led  # Cambia el estado
+            led.value(estado_led)        # Actualiza el LED
+
